301f1d60e3a09c81453e2e89ad5a20324cb913ed
Sergio Ruiz <60074318+sergio8999@users.noreply.github.com>
2020-10-30 13:39:16 +0100

    Subida touch

---
+++ touch.js
@@ -0,0 +1,38 @@
+function inicio() {
+    var el = document.getElementsByTagName("canvas")[0];
+    el.addEventListener("touchstart", handleStart, false);
+    el.addEventListener("touchend", handleEnd, false);
+    //el.addEventListener("touchcancel", handleCancel, false);
+    //el.addEventListener("touchleave", handleLeave, false);
+    el.addEventListener("touchmove", handleMove, false);
+    el.addEventListener('click', click);
+
+    if (window.DeviceOrientationEvent){
+      window.addEventListener("deviceorientation", orientacion);
+  }
+  }
+
+  window.onload = inicio;
+
+  function handleStart(evt) {
+    evt.preventDefault();
+    document.getElementById("log").innerHTML = "Touchstart " + evt.touches[0].pageX +" "+evt.touches[0].pageY ;
+  }
+
+  function handleEnd(evt) {
+    evt.preventDefault();
+    document.getElementById("log").innerHTML = "Touchend "+ evt.touches[0].pageX +" "+evt.touches[0].pageY;
+  }
+
+  function handleMove(evt) {
+    evt.preventDefault();
+    document.getElementById("log").innerHTML = "Touchmove "+ evt.touches[0].pageX +" "+evt.touches[0].pageY;
+  }
+
+  function click(){
+      alert('click');
+  }
+
+  function orientacion(e){
+    document.getElementById("log").innerHTML = e.alpha;
+  }
